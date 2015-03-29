a7c4e568c0a51106c27d1d289a4edfd74d5db8ad
Adilx05 <adilx05@gmail.com>
2015-03-29 16:13:25 +0300

    Fixed Cpu

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Timers;
+using System.Threading;
 
 namespace F5Basıcı
 {
@@ -13,30 +14,30 @@ namespace F5Basıcı
     {
         static void Main(string[] args)
               {
-      System.Timers.Timer t = new System.Timers.Timer(900 * 1000);
+      System.Timers.Timer t = new System.Timers.Timer(900000);
       t.Elapsed += new ElapsedEventHandler(Yenileme);
       t.Elapsed += new ElapsedEventHandler(Yaz);
       t.Elapsed += new ElapsedEventHandler(Zaman);
       t.Start();
 
       while(true)
-      {
-      
+      { // 1 Saniyelik Uyku
+          Thread.Sleep(1000);
       }
                 }
 
          static void Yenileme(object o, ElapsedEventArgs a)
-   {
+   { // Gönderilen Tuş
      int hwnd = FindWindow(null, "League Of Legends");
             SetForegroundWindow(hwnd);
             SendKeys.SendWait("{F5}");
    }
         static void Yaz(object o, ElapsedEventArgs a)
-         {
-             Console.WriteLine("Last Press Time");
+         {// Son Gönderilme Zamanı
+             Console.WriteLine("Son Gönderme Zamanı");
          }
         static void Zaman(object o, ElapsedEventArgs a)
-        {
+        {// Son Gönderilme Tarihi
             Console.WriteLine(DateTime.Now);
         }
         [DllImport("user32.dll")]
